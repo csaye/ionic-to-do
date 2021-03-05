f2fcd085de077879e2959475f42d372d0cef7327
Cooper Saye <coopersaye@gmail.com>
2021-03-05 15:23:26 -0700

    Restructure Home component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,13 @@
 import './App.css';
 
 import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import { firebaseConfig } from '../../util/firebaseConfig.js';
-import { useAuthState } from 'react-firebase-hooks/auth';
 
 import NewTodo from '../NewTodo/NewTodo';
 import TodoList from '../TodoList/TodoList';
 import SignIn from '../SignIn/SignIn';
 
-// initialize firebase
-firebase.initializeApp(firebaseConfig);
-
 // App component
 function App() {
-  useAuthState(firebase.auth());
-
   return (
     <div className="App">
       {
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,28 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import App from '../../components/App/App';
+import SignOut from '../../components/SignOut/SignOut';
 import './Home.css';
 
-const Home: React.FC = () => {
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { firebaseConfig } from '../../util/firebaseConfig.js';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+function Home() {
+  useAuthState(firebase.auth());
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic To Do</IonTitle>
+          <div className="toolbar-items">
+            <IonTitle>Ionic To Do</IonTitle>
+            <SignOut />
+          </div>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
